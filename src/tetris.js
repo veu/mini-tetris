65638858a2d304a62f6780c285bbfc52b84df1e5
Becky <becky@monometric.net>
2017-10-05 23:37:43 +0200

    582

--- src/tetris.js
+++ src/tetris.js
@@ -1,5 +1,5 @@
 A=e=>e.map((x,i)=>i%2?0:b[H|=x<0|x>9|b[y=e[i+1]]>>x&1|y>17,y]|=1<<x,p=e,H=0,b=[...B])&&H,
-R=e=>A(P=[...'716160504030312120'.substr(new Date%6*2,8)].map(eval),T=200),
+R=e=>A(P=[...'716160504030312120'.substr(new Date%6*2,k=8)].map(eval)),
 R(B=Array(18).fill(S=0)),
 D=e=>(
     $=S,
@@ -10,15 +10,13 @@ D=e=>(
     (A(P.map((v,i)=>v+i%2))
         ?R(A(P),D(B=[s=0,0,0,0].concat(b).filter(v=>v-1023||!(S+=++s)).slice(-18)))
         :!D(b,P=p)
-    )||setTimeout(_,T)
+    )||setTimeout(_,k-2?200:20)
 )(
 onkeydown=e=>
-    (k=e.keyCode-38)-2
-        ?(k-1&&~k
-            ?k||A(P.map((v,i)=>P[2]+(i%2?P[3]-P[i-1]:P[i+1]-P[3])))
-            :A(P.map((v,i)=>v-~i%2*k))
-        )
-            ?0
-            :P=p
-        :T=20
+    ((k=e.keyCode-38)-1&&~k
+        ?k||A(P.map((v,i)=>P[2]+(i%2?P[3]-P[i-1]:P[i+1]-P[3])))
+        :A(P.map((v,i)=>v-~i%2*k))
+    )
+        ?0
+        :P=p
 )
