982a3726a595722166cf4927cd21ee56f47dc0f3
Becky <becky@monometric.net>
2017-10-05 19:13:21 +0200

    add compile step

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const gulp = require('gulp');
+const replace = require('gulp-replace');
+const size = require('gulp-size');
+
+gulp.task('default', ['compile'], function () {
+    gulp.watch('src/*', ['compile']);
+});
+
+gulp.task('compile', function () {
+  const js = fs.readFileSync('src/tetris.js', 'utf8');
+  const jsCompiled = js.trim().replace(/\s*\n\s*/g, '');
+  return gulp.src('src/tetris.html')
+    .pipe(replace(/<demo>/, jsCompiled))
+    .pipe(size({pretty: false, showFiles: true}))
+    .pipe(gulp.dest('dist'));
+});
