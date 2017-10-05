c09c2476d5aacc046e957e011e98029e65b2d943
Becky <becky@monometric.net>
2017-10-05 20:03:09 +0200

    654

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ gulp.task('compile', function () {
   const js = fs.readFileSync('src/tetris.js', 'utf8');
   const jsCompiled = js.trim().replace(/\s*\n\s*/g, '');
   return gulp.src('src/tetris.html')
+    .pipe(replace(/\s*\n\s*/g, ''))
     .pipe(replace(/<demo>/, jsCompiled))
     .pipe(size({pretty: false, showFiles: true}))
     .pipe(gulp.dest('dist'));
