7e2ac514854766cb3fbececaeeab915a520a85b8
Becky <becky@monometric.net>
2017-10-06 23:34:34 +0200

    538

--- src/tetris.js
+++ src/tetris.js
@@ -13,22 +13,22 @@
 // s – Tally of current move used to reward clearing multiple lines
 // I - Offset of next piece
 
-M=e=>A(P.map((v,i)=>eval(e))),
+M=e=>A(P.map((v,i)=>eval(e[0]))),
 A=e=>e.some((x,i)=>i%2?0:(b[h=x<0|x>9|b[y=e[i+1]]>>x&1|y>17,y]|=1<<x,h),p=e,b=[...B]),
 B=Array(18).fill(S=P=I=0),
 D=e=>b.map(v=>b.map((n,x)=>x>9?0:Z.innerText=$+='□■'[v>>x&1],$+='\n'),$=S),
 (_=e=>
     A(P=P||[...'202131304050606171'.substr(I%6*2,k=8)])
     ?0:I=setTimeout(_,k-2?200:20,
-        M('+v+i%2')
+        M`+v+i%2`
             ?A(P)&D(B=[s=P=0,0,0,0,...b].filter(v=>v-1023||!(S+=++s)).slice(-18))
             :D(P=p)
     )
 )(
 onkeydown=e=>
     P?((k=e.which-38)-1&&~k
-        ?k||M('P[2]-(q=1-i%2*2)*(P[3]-P[i+q])')
-        :M('v-~i%2*k')
+        ?k||M`P[2]-(q=1-i%2*2)*(P[3]-P[i+q])`
+        :M`v-~i%2*k`
     )
     ||D(P=p):0
 )
