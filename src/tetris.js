0148bbfb6e4fc2e1cb413f32dfcf68e76e0c005f
Rebecca König <a.koenig@rebuy.de>
2017-10-06 10:33:11 +0200

    570

--- src/tetris.js
+++ src/tetris.js
@@ -2,7 +2,7 @@ A=e=>e.map((x,i)=>i%2?0:b[H|=x<0|x>9|b[y=e[i+1]]>>x&1|y>17,y]|=1<<x,p=e,H=0,b=[.
 B=Array(18).fill(S=P=0),
 D=e=>(
     $=S,
-    e.map(v=>e.map((n,x)=>x>9?0:$+='□■'[v>>x&1],$+='<br>')),
+    b.map(v=>b.map((n,x)=>x>9?0:$+='□■'[v>>x&1],$+='<br>')),
     Z.innerHTML=$
 ),
 (_=e=>
@@ -10,7 +10,7 @@ D=e=>(
     ||setTimeout(_,k-2?200:20,
         A(P.map((v,i)=>v+i%2))
             ?A(P)&D(B=[s=P=0,0,0,0,...b].filter(v=>v-1023||!(S+=++s)).slice(-18))
-            :D(b,P=p)
+            :D(P=p)
     )
 )(
 onkeydown=e=>
@@ -18,6 +18,5 @@ onkeydown=e=>
         ?k||A(P.map((v,i)=>P[2]+(i%2?P[3]-P[i-1]:P[i+1]-P[3])))
         :A(P.map((v,i)=>v-~i%2*k))
     )
-        ?0
-        :D(b,P=p)
+    ||D(P=p)
 )
