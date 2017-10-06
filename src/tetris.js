468537291d4bcc731fee3c70ed969cf61afe0083
Becky <becky@monometric.net>
2017-10-06 23:45:06 +0200

    532

--- src/tetris.js
+++ src/tetris.js
@@ -1,8 +1,7 @@
 // _() – Main loop
-// A(piece) – Copy piece to p and merge field B and piece into field b.
-//            Return true if piece overlapped with B
 // D() – Draw updated field b
-// M(code) – Map P by evaluating code and call A with the result
+// M(code) – Map P to p using code and merge it with field B into field b.
+//           Return true if piece overlaps with B.
 
 // B – Current state of playing field, format: [c00<<0|c01<<1|…, c10<<0|c11<<1|…, …]
 // b - Updated state of playing field
@@ -13,15 +12,14 @@
 // s – Tally of current move used to reward clearing multiple lines
 // I - Offset of next piece
 
-M=e=>A(P.map((v,i)=>eval(e[0]))),
-A=e=>e.some((x,i)=>i%2?0:(b[h=x<0|x>9|b[y=e[i+1]]>>x&1|y>17,y]|=1<<x,h),p=e,b=[...B]),
+M=e=>(p=P.map((v,i)=>eval(e[0]),b=[...B])).some((x,i)=>i%2?0:(b[h=x<0|x>9|b[y=p[i+1]]>>x&1|y>17,y]|=1<<x,h)),
 B=Array(18).fill(S=P=I=0),
 D=e=>b.map(v=>b.map((n,x)=>x>9?0:Z.innerText=$+='□■'[v>>x&1],$+='\n'),$=S),
 (_=e=>
-    A(P=P||[...'202131304050606171'.substr(I%6*2,k=8)])
+    (P=P||[...'202131304050606171'.substr(I%6*2,k=8)],M`v`)
     ?0:I=setTimeout(_,k-2?200:20,
         M`+v+i%2`
-            ?A(P)&D(B=[s=P=0,0,0,0,...b].filter(v=>v-1023||!(S+=++s)).slice(-18))
+            ?M`v`&D(B=[s=P=0,0,0,0,...b].filter(v=>v-1023||!(S+=++s)).slice(-18))
             :D(P=p)
     )
 )(
