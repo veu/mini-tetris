83f0844a450c8ebfe24ee9b55c44f7847beabdfd
Becky <becky@monometric.net>
2017-10-06 23:05:50 +0200

    556

--- src/tetris.js
+++ src/tetris.js
@@ -12,21 +12,21 @@
 // s – Tally of current move used to reward clearing multiple lines
 // I - Offset of next piece
 
-A=e=>e[M]((x,i)=>i%2?0:b[h|=x<0|x>9|b[y=e[i+1]]>>x&1|y>17,y]|=1<<x,p=e,h=0,b=[...B])&&h,
+A=e=>e.some((x,i)=>i%2?0:(b[h=x<0|x>9|b[y=e[i+1]]>>x&1|y>17,y]|=1<<x,h),p=e,b=[...B]),
 B=Array(18).fill(S=P=I=0),
-D=e=>b[M](v=>b[M]((n,x)=>x>9?0:Z.innerText=$+='□■'[v>>x&1],$+='\n'),$=S),
+D=e=>b.map(v=>b.map((n,x)=>x>9?0:Z.innerText=$+='□■'[v>>x&1],$+='\n'),$=S),
 (_=e=>
-    A(P=P||[...'202131304050606171'.substr(I%6*2,k=8)][M='map'](eval))
+    A(P=P||[...'202131304050606171'.substr(I%6*2,k=8)].map(eval))
     ?0:I=setTimeout(_,k-2?200:20,
-        A(P[M]((v,i)=>v+i%2))
+        A(P.map((v,i)=>v+i%2))
             ?A(P)&D(B=[s=P=0,0,0,0,...b].filter(v=>v-1023||!(S+=++s)).slice(-18))
             :D(P=p)
     )
 )(
 onkeydown=e=>
     P?((k=e.which-38)-1&&~k
-        ?k||A(P[M]((v,i)=>P[2]+(q=i%2*2-1)*(P[3]-P[i-q])))
-        :A(P[M]((v,i)=>v-~i%2*k))
+        ?k||A(P.map((v,i)=>P[2]+(q=i%2*2-1)*(P[3]-P[i-q])))
+        :A(P.map((v,i)=>v-~i%2*k))
     )
     ||D(P=p):0
 )
